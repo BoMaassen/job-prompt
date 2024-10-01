f8ab4d78412757f05a22434f8be9ce1e6f0f9a01
BoMaassen <bomaassen@live.nl>
2024-10-01 17:43:58 +0200

    opdracht 1 en 2 af

--- scripts/main.js
+++ scripts/main.js
@@ -72,3 +72,22 @@ const departments = {
 }
 
 console.log(departments);
+console.log("De afdeling Sales heeft " + departments.sales.numberOfEmployees + " medewerkers");
+console.log("Marketing is een leuke afdeling om te werken. " + departments.marketing.description);
+console.log("De afdeling Customer Service heeft " + departments["customer-service"].numberOfEmployees + " medewerkers");
+console.log("Sales is een uitdagende afdeling om te werken als Verkoopmanager. " + departments.sales.jobs[0].description);
+const userInput = prompt("Over welke afdeling wil je meer informatie? Kies uit: [marketing / sales / customer service]");
+console.log(userInput);
+
+if(userInput === "marketing"){
+    console.log("Je koos " + userInput + ": " + departments.marketing.description);
+}
+else if(userInput === "sales"){
+    console.log("Je koos " + userInput + ": " + departments.sales.description);
+}
+else if(userInput === "customer service"){
+    console.log("Je koos " + userInput + ": " + departments["customer-service"].description);
+}
+else {
+    console.error("Ongeldige keuze")
+}
