376369bc21d86b7b17cf271e21d8696cf5df20a8
BoMaasen <bomaassen@live.nl>
2024-10-02 17:23:50 +0200

    opdracht 3 en 4 af

--- scripts/main.js
+++ scripts/main.js
@@ -72,11 +72,16 @@ const departments = {
 }
 
 console.log(departments);
+// ------- OPDRACHT 1 -------//
+
 console.log("De afdeling Sales heeft " + departments.sales.numberOfEmployees + " medewerkers");
 console.log("Marketing is een leuke afdeling om te werken. " + departments.marketing.description);
 console.log("De afdeling Customer Service heeft " + departments["customer-service"].numberOfEmployees + " medewerkers");
 console.log("Sales is een uitdagende afdeling om te werken als Verkoopmanager. " + departments.sales.jobs[0].description);
+
+// ------- OPDRACHT 2 -------//
 const userInput = prompt("Over welke afdeling wil je meer informatie? Kies uit: [marketing / sales / customer service]");
+
 console.log(userInput);
 
 if(userInput === "marketing"){
@@ -91,3 +96,28 @@ else if(userInput === "customer service"){
 else {
     console.error("Ongeldige keuze")
 }
+
+// ------- OPDRACHT 4 -------//
+
+console.log(userInput + " is een leuke afdeling om te werken. Er werken op dit moment " + departments[userInput].numberOfEmployees + " medewerkers.");
+
+
+// ------- OPDRACHT 3 -------//
+
+const jobDescription = prompt("Je koos " + userInput + ". \n Over welke functie wil je meer weten? Voer een getal tussen 0 en 3 in. \n0: " + departments[userInput].jobs[0].title + ",\n1: " + departments[userInput].jobs[1].title + ",\n2: " + departments[userInput].jobs[2].title + ",\n3: " + departments[userInput].jobs[3].title);
+
+console.log("jobDescription", jobDescription);
+
+if (jobDescription === "0") {
+    console.log("Je koos", departments[userInput].jobs[0].title + ". Een uitdagende rol!", departments[userInput].jobs[0].description);
+} else if (jobDescription === "1") {
+    console.log("Je koos", departments[userInput].jobs[1].title + ". Een uitdagende rol!", departments[userInput].jobs[1].description);
+} else if (jobDescription === "2") {
+    console.log("Je koos", departments[userInput].jobs[2].title + ". Een uitdagende rol!", departments[userInput].jobs[2].description);
+} else if (jobDescription === "3") {
+    console.log("Je koos", departments[userInput].jobs[3].title + ". Een uitdagende rol!", departments[userInput].jobs[3].description);
+} else {
+    console.log("Ongeldige keuze");
+}
+
+
